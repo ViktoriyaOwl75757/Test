f20ddbcc5c07448389057642e0ca8e5f45310af9
ViktoriyaOwl75757 <viktoriya77711@gmail.com>
2020-02-23 22:51:32 +0300

    Задание 4

---
+++ src/TableMult.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TableMult {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число от 1 до 10");
+        int num = in.nextInt();
+        System.out.println(num*1);
+        System.out.println(num*2);
+        System.out.println(num*3);
+        System.out.println(num*4);
+        System.out.println(num*5);
+        System.out.println(num*6);
+        System.out.println(num*7);
+        System.out.println(num*8);
+        System.out.println(num*9);
+        System.out.println(num*10);
+
+    }
+}
+//без циклов,все как положено
